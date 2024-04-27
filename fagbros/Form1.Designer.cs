b73986c91b98d746e599430e2b0c8cd85e134131
Ferdinan Kurnia <155617411+ferdinankurnian@users.noreply.github.com>
2024-04-27 13:15:22 +0700

    Add project files.

---
+++ fagbros/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace fagbros
+{
+    partial class formLevel1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // formLevel1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 450);
+            this.Name = "formLevel1";
+            this.Text = "FAG Bros - Level 1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ fagbros/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fagbros
+{
+    public partial class formLevel1 : Form
+    {
+        public formLevel1()
+        {
+            InitializeComponent();
+        }
+    }
+}
